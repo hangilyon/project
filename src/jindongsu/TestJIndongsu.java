92da1265ed26d12744e7a20717001f74016393da
jindongsu <dsjin123@naver.com>
2021-08-26 16:39:02 +0900

    진동수 작성중

---
+++ src/jindongsu/TestJIndongsu.java
@@ -0,0 +1,8 @@
+package jindongsu;
+
+public class TestJIndongsu {
+	public void disp() {
+		System.out.println("진동수 클래스 입니다");
+	}
+
+}
--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -1,18 +1,22 @@
 package main;
 
 import java.util.Scanner;
+import jindongsu.TestJIndongsu;
 
 public class MainClass {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int num;
+		TestJIndongsu tdongsu = new TestJIndongsu();
 		while(true) {
 			System.out.println("1.한길용 2.이진원 3.진동수 4.이창훈 5.이동호");
 			num = input.nextInt();
 			switch(num) {
 			case 1 : break;
 			case 2 : break;
-			case 3 : break;
+			case 3 : 
+					 tdongsu.disp();
+					 break;
 			case 4 : break;
 			case 5 : break;
 			}
